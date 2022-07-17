88bc46143999185dec75c9e1fe7d16cd17110456
Tushar Marewad <tusharmarewad96@gmail.com>
2022-07-17 10:32:09 +0530

    updated

--- App.js
+++ App.js
@@ -1,1 +1,57 @@
 
+var seconds = 0;
+var tens = 0;
+
+var appendTens = document.getElementById("tens")
+var appendSeconds = document.getElementById("seconds")
+var buttonStart = document.getElementById("button-start");
+var buttonStop = document.getElementById("button-stop");
+var buttonReset = document.getElementById("button-reset");
+
+var Interval;
+
+buttonStart.onclick = function () {
+    clearInterval(Interval);
+
+    Interval = setInterval(timerEngine, 10);
+}
+
+buttonStop.onclick = function () {
+
+    clearInterval(Interval);
+}
+
+
+
+buttonReset.onclick = function () {
+    clearInterval(Interval);
+    tens = 0;
+    seconds = 0;
+    appendTens.innerHTML = "00";
+    appendSeconds.innerHTML = "00";
+}
+
+
+function timerEngine() {
+    tens++;
+
+    if (tens <= 9) {
+
+        appendTens.innerHTML = "0" + tens;
+    }
+
+    if (tens > 9) {
+        appendTens.innerHTML = tens;
+    }
+
+    if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "00";
+    }
+
+    if (seconds > 9) {
+        appendSeconds.innerHTML = seconds;
+    }
+}
